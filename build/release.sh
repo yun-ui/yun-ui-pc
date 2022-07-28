8f79ce3e9a863b65e523e6864d8a3bc0a5605317
yehan_zhou <yehan_zhou@kingdee.com>
2022-07-28 18:02:44 +0800

    fix: release shell

--- build/release.sh
+++ build/release.sh
@@ -36,11 +36,11 @@ then
   npm version $VERSION --message "[release] $VERSION"
 
   # publish
-  git push eleme master
-  git push eleme refs/tags/v$VERSION
+  git push origin master
+  git push origin refs/tags/v$VERSION
   git checkout dev
   git rebase master
-  git push eleme dev
+  git push origin dev
 
   if [[ $VERSION =~ "beta" ]]
   then
